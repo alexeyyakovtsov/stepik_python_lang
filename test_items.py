49329e91cb58b54f153bd8cb8794b52861b7e2cd
Alexey Yakovtsov <hannisolo@yandex.ru>
2020-11-30 16:29:36 +0300

    update checks

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ def test_check_catalogue_item(browser):
     browser.get(link)
     browser.implicitly_wait(4)
     btn = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert btn.is_displayed()
+    assert btn.is_displayed() == True, "the button did not find"
