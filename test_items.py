a9a8a8613c0528882738b88260141826adf5f6b7
Alexey Yakovtsov <hannisolo@yandex.ru>
2020-11-30 16:25:25 +0300

    Add new test: Check button

---
+++ test_items.py
@@ -0,0 +1,8 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_catalogue_item(browser):
+    browser.get(link)
+    browser.implicitly_wait(4)
+    btn = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+    assert btn.is_displayed()
